0fc1325c2b8e8ca65b535864d3d02888693abde1
runtimerobotics <runtimerobotics@gmail.com>
2024-11-15 16:08:18 +0530

    Updated create container script to work with realsense and without it

--- docker/create_container.sh
+++ docker/create_container.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-CONTAINER_NAME="mycobot_sim_container"  # Name of the container you want to create.
+CONTAINER_NAME="mycobot_sim_container"  # Name of the container to create.
 DOCKER_IMAGE="mycobot/sim:v0.0.1"  # Name of the Docker image.
 HOST_WS_PATH="/home/$USER/mycobot_ws/src"  # Path to your workspace on the host.
 
@@ -22,44 +21,81 @@ if [ ! -f "$XAUTH" ]; then
     chmod a+r "$XAUTH"
 fi
 
-docker rm "$CONTAINER_NAME"
+# Remove existing container
+docker rm -f "$CONTAINER_NAME"
+
+BUS=$(lsusb | grep 8086 | cut -d " " -f 2)
+PORT=$(lsusb | grep 8086 | cut -d " " -f 4 | cut -d ":" -f 1)
 
 # Check for NVIDIA GPU
 if nvidia-smi | grep -q NVIDIA; then
     echo "NVIDIA GPU detected, initializing container with GPU support"
-    docker run -it --network host \
-        --privileged \
-        --name "$CONTAINER_NAME" \
-        --gpus all \
-        --runtime nvidia \
-        -p 27017:27017 \
-        -v ~/mongodb7_data:/data/db \
-        --env="DISPLAY=$DISPLAY" \
-        --env="QT_X11_NO_MITSHM=1" \
-        --env="NVIDIA_DRIVER_CAPABILITIES=all" \
-        --volume="/etc/timezone:/etc/timezone:ro" \
-        --volume="/etc/localtime:/etc/localtime:ro" \
-        --volume="$XSOCK:$XSOCK:rw" \
-        --volume="$XAUTH:$XAUTH_DOCKER:rw" \
-        --volume="$HOST_WS_PATH:/home/mycobot/mycobot_ws/src:rw" \
-        "$DOCKER_IMAGE" \
-        bash
+    if [ ! -z "$PORT" ]; then
+        echo "NVIDIA with RealSense"
+        docker run -it --network host \
+            --privileged \
+            --name "$CONTAINER_NAME" \
+            --gpus all \
+            --runtime nvidia \
+            --env="DISPLAY=$DISPLAY" \
+            --env="QT_X11_NO_MITSHM=1" \
+            --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+            --volume="/etc/timezone:/etc/timezone:ro" \
+            --volume="/etc/localtime:/etc/localtime:ro" \
+            --volume="/dev/bus/usb/$BUS/$PORT:/dev/bus/usb/$BUS/$PORT" \
+            --device-cgroup-rule "c 189:* rmw" \
+            --volume="$XSOCK:$XSOCK:rw" \
+            --volume="$XAUTH:$XAUTH_DOCKER:rw" \
+            --volume="$HOST_WS_PATH:/home/mycobot/mycobot_ws/src:rw" \
+            "$DOCKER_IMAGE" \
+            bash
+    else
+        echo "NVIDIA without RealSense"
+        docker run -it --network host \
+            --privileged \
+            --name "$CONTAINER_NAME" \
+            --gpus all \
+            --runtime nvidia \
+            --env="DISPLAY=$DISPLAY" \
+            --env="QT_X11_NO_MITSHM=1" \
+            --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+            --volume="/etc/timezone:/etc/timezone:ro" \
+            --volume="/etc/localtime:/etc/localtime:ro" \
+            --volume="$XSOCK:$XSOCK:rw" \
+            --volume="$XAUTH:$XAUTH_DOCKER:rw" \
+            --volume="$HOST_WS_PATH:/home/mycobot/mycobot_ws/src:rw" \
+            "$DOCKER_IMAGE" \
+            bash
+    fi
 else
     echo "NVIDIA GPU NOT detected, initializing container without GPU support"
-    docker run -it --network host \
-        --privileged \
-        --name "$CONTAINER_NAME" \
-        --env="DISPLAY=$DISPLAY" \
-        --volume="$XSOCK:$XSOCK:rw" \
-        --volume="$XAUTH:$XAUTH_DOCKER:rw" \
-        -p 27017:27017 \
-        -v ~/mongodb7_data:/data/db \
-        --volume="/etc/timezone:/etc/timezone:ro" \
-        --volume="/etc/localtime:/etc/localtime:ro" \
-        --volume="$HOST_WS_PATH:/home/mycobot/mycobot_ws/src:rw" \
-        "$DOCKER_IMAGE" \
-        bash
+    if [ ! -z "$PORT" ]; then
+        echo "No NVIDIA with RealSense"
+        docker run -it --network host \
+            --privileged \
+            --name "$CONTAINER_NAME" \
+            --env="DISPLAY=$DISPLAY" \
+            --volume="$XSOCK:$XSOCK:rw" \
+            --volume="$XAUTH:$XAUTH_DOCKER:rw" \
+            --volume="/etc/timezone:/etc/timezone:ro" \
+            --volume="/etc/localtime:/etc/localtime:ro" \
+            --volume="/dev/bus/usb/$BUS/$PORT:/dev/bus/usb/$BUS/$PORT" \
+            --device-cgroup-rule "c 189:* rmw" \
+            --volume="$HOST_WS_PATH:/home/mycobot/mycobot_ws/src:rw" \
+            "$DOCKER_IMAGE" \
+            bash
+    else
+        echo "No NVIDIA with No RealSense"
+        docker run -it --network host \
+            --privileged \
+            --name "$CONTAINER_NAME" \
+            --env="DISPLAY=$DISPLAY" \
+            --volume="$XSOCK:$XSOCK:rw" \
+            --volume="$XAUTH:$XAUTH_DOCKER:rw" \
+            --volume="/etc/timezone:/etc/timezone:ro" \
+            --volume="/etc/localtime:/etc/localtime:ro" \
+            --volume="$HOST_WS_PATH:/home/mycobot/mycobot_ws/src:rw" \
+            "$DOCKER_IMAGE" \
+            bash
+    fi
 fi
-
-#        --volume="/dev:/dev" \
-
